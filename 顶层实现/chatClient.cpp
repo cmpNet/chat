6c48edd577f300206a94090ce4a3cfcb6bc6dfcf
刘忍 <18826073587@163.com>
2016-10-08 22:55:35 +0800

    封装 POST 函数

--- 顶层实现/chatClient.cpp
+++ 顶层实现/chatClient.cpp
@@ -23,7 +23,9 @@ bool welcome() {
   }
 }
 
-void signout() {
+// 封装的 POST 方法，第一个参数为发往服务器的信息，第二个参数为接受服务器的信息
+void post(char *sendMessage, char *receiveMessage) {
+  int bufferSize = 1024;
   int port = 2014;
   int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in serverAddress;
@@ -34,24 +36,25 @@ void signout() {
   connect(clientSocket,
           (struct sockaddr*)&serverAddress,
           sizeof(serverAddress));
-
   // 向服务器发送数据
+  send(clientSocket, sendMessage, strlen(sendMessage), 0);
+  // 读回服务器的数据
+  read(clientSocket, receiveMessage, bufferSize);
+  // 关闭客户端
+  close(clientSocket);
+}
+
+void signout() {
   int bufferSize = 1024;
   char sendMessage[] = "SignoutRequest";
-  send(clientSocket, sendMessage, sizeof(sendMessage), 0);
-
-  // 读回服务器的数据
   char receiveMessage[bufferSize];
-  read(clientSocket, receiveMessage, sizeof(receiveMessage) - 1);
+  post(sendMessage, receiveMessage);
   if (strcmp(receiveMessage, "Bye") == 0) {
     printf("Sign out successfully!\n");
     exit(0);
   } else {
     printf("[ERROR] Sign out failed! Please retry.\n");
   }
-
-  // 关闭客户端
-  close(clientSocket);
 }
 
 void operate() {
@@ -67,33 +70,14 @@ void operate() {
 }
 
 void signin() {
-  int port = 2014;
-  int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in serverAddress;
-  memset(&serverAddress, 0, sizeof(serverAddress));
-  serverAddress.sin_family = AF_INET;  // IPv4
-  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");  // 服务器
-  serverAddress.sin_port = htons(port);  // 端口
-  connect(clientSocket,
-          (struct sockaddr*)&serverAddress,
-          sizeof(serverAddress));
-
-  // 向服务器发送数据
   int bufferSize = 1024;
   char sendMessage[] = "SigninRequest";
-  send(clientSocket, sendMessage, sizeof(sendMessage), 0);
-
-  // 读回服务器的数据
   char receiveMessage[bufferSize];
-  read(clientSocket, receiveMessage, sizeof(receiveMessage) - 1);
+  post(sendMessage, receiveMessage);
   printf("\nSign in successfully! The online clients are following:\n");
   printf("+-----------+----------------------+\n");
   printf("| Index     | Address              |\n");
   printf("+-----------+----------------------+\n");
-
-  // 关闭客户端
-  close(clientSocket);
-
   // 解析数据
   const char *split = "|";
   char *p;
@@ -112,14 +96,10 @@ void signin() {
 int main() {
   if (!welcome())
     return 0;
-
-  // 创建客户端
-  printf("- Creating a client service...\n");
-
   // 登录
+  printf("- Creating a client service...\n");
   printf("- Receiving message from server...\n");
   printf("- Exit automatically if failed.\n");
   signin();
-
   return 0;
 }
